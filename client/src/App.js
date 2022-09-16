ad58bb396b9534709bcd67c18dcc7143525f04fa
Kathleen Prendergast <katprendergast00@gmail.com>
2022-09-16 10:11:51 -0400

    Change react main page

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
+        <p>I have made an update to the website!!</p>
       </header>
     </div>
   );
